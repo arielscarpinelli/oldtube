f621c05d608c209e30b06f0e103bbd91647f5819
Mynacol <Mynacol@users.noreply.github.com>
2020-09-13 14:53:57 +0200

    Update development dependencies
    webpack 3 -> 4
    -> requires webpack-cli
    babel-loader 7 -> 8
    babel 6 -> 7
    -> preset-env replaces deprecated preset-es2015
    -> plugin-proposal-class-properties is sufficient to replace deprecated preset-stage-0
    babel-preset-react 6 -> 7
    file-loader 0.11.2 -> 6
    
    Move config for babel from .babelrc to webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
           'miniget',
           'ytdl-core'
         ]),
-        loader: "babel-loader" },
+        use: {
+        loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
       { test: /\.css$/, loader: "file-loader?name=[name].[ext]" }
     ]
   }
